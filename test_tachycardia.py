f83752b8b8be21c0a0e32577a6db82a2cc7148e7
Janet Chen <janetchen@Janets-MacBook-Air.local>
2019-02-06 14:15:44 -0500

    trigger build

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -2,11 +2,11 @@ from tachycardia import is_tachycardic
 import pytest
 
 
-@pytest.mark.parametrize("input, exp",
+@pytest.mark.parametrize("inputs, exp",
                          [('tachycardic', True), ('  tachycardic', True),
                           ('tachycardic', True), ('tachycardic.', True),
                           ('tachycardi', False), ('TachyCardic', True),
                           ('TACHYCARDIC', True), (6, False)])
-def test_is_tachycardic(input, exp):
-    ans = is_tachycardic(input)
+def test_is_tachycardic(inputs, exp):
+    ans = is_tachycardic(inputs)
     assert ans == exp
