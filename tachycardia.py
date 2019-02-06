9bc392285cc21f101a3f489850685d2068713985
Janet Chen <janetchen@Janets-MacBook-Air.local>
2019-02-06 14:09:30 -0500

    pep8 compliant version

--- tachycardia.py
+++ tachycardia.py
@@ -2,8 +2,8 @@ import re
 
 
 def is_tachycardic(string_in):
-    if isinstance(string_in,str):
-        string=string_in.lower()
+    if isinstance(string_in, str):
+        string = string_in.lower()
     else:
         return False
     if re.search('tachycardic', string):
