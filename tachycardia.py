66cf72886bffbba32a6a793af18a25667c482a6a
Janet Chen <janetchen@Janets-MacBook-Air.local>
2019-02-06 14:07:24 -0500

    rejects non-string inputs

--- tachycardia.py
+++ tachycardia.py
@@ -2,6 +2,10 @@ import re
 
 
 def is_tachycardic(string_in):
+    if isinstance(string_in,str):
+        string=string_in.lower()
+    else:
+        return False
     if re.search('tachycardic', string):
         return True
     else:
